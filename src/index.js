ce3a9947f852602e39cb26b46dbd0c13ca8aa3e5
Mr.Resor <jakusienko@gmail.com>
2023-11-23 17:29:30 +0100

    Implemented first of the endpoints, started work on the second.

--- src/index.js
+++ src/index.js
@@ -15,13 +15,36 @@ const STREAM_COUNT = {};
 APP.use(EXPRESS.json());
 
 APP.post('/api/stream/open', (req, res) => {
-  console.log(req.body);
-  console.log(req.body['test']);
-  res.send({"Over_Limit": false});
+  if (req.body['user'] === undefined) {
+    // Checking if user field is in body just in case, but no validation of
+    // user is done here as per task contents (in practical use, it would
+    // probably be in the latter part).
+    console.log("Reqest without user field.") // TODO: Change to logging.
+    res.statusCode = 400;
+    res.send();
+  }
+  const user = req.body['user'];
+  if (STREAM_COUNT[user] === undefined) {
+    // if user is not tracked at the moment, add him to the dictionary.
+    STREAM_COUNT[user] = 0;
+  }
+  // returns false if user has 2 or less streams opened, true otherwise.
+  res.send({ "Over_Limit": (STREAM_COUNT[user] > 2) });
+  // no need to keep adding after 3, it is better to bound it
+  STREAM_COUNT[user] += (0 + (STREAM_COUNT[user] < 3));
+  console.log(STREAM_COUNT);
 });
 
 APP.post('/api/stream/close', (req, res) => {
-  res.send('Files');
+  if (req.body['user'] === undefined) {
+    // Checking if user field is in body just in case, but no validation of
+    // user is done here as per task contents (in practical use, it would
+    // probably be in the "else" part).
+    console.log("Reqest without user field.") // TODO: Change to logging.
+    res.statusCode = 400;
+    res.send();
+  }
+  
 });
 
 APP.listen(PORT, () => {
